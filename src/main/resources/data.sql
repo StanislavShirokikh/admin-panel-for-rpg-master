c06b8f2dee0772ebc81f918099516a3691bed158
StanislavShirokikh <shirokikhstanislav@icloud.com>
2023-12-19 20:29:56 +0700

    структура базы данных

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO race(name)
+VALUES (HUMAN),
+       (DWARF),
+       (ELF),
+       (GIANT),
+       (ORC),
+       (TROLL),
+       (HOBBIT);
+
+INSERT INTO profession(name)
+VALUES (WARRIOR),
+    (ROGUE),
+    (SORCERER),
+    (CLERIC),
+    (PALADIN),
+    (NAZGUL),
+    (WARLOCK),
+    (DRUID);
